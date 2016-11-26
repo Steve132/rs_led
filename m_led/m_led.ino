68b3cffd197a545864b7c75a9afcfa78552cb609
Steve132 <steve@soapforge.com>
2016-11-26 06:57:54 -0500

    Added multiple patterns, added brightness controls.  Works

--- m_led/m_led.ino
+++ m_led/m_led.ino
@@ -9,14 +9,6 @@ static BaseLedStripRS* strips[]=
 
 static const int num_strips=sizeof(strips)/sizeof(strips[0]);
 
-void setup() 
-{              
-	for(int i=0;i<num_strips;i++)
-	{
-		strips[i]->setup();
-	}
-}
-
 void send_color_all(uint32_t color)
 {
 	for(int i=0;i<num_strips;i++)
@@ -25,36 +17,103 @@ void send_color_all(uint32_t color)
 	}
 }
 
-typedef struct pattern_ts
+uint32_t brighten(uint32_t color,uint8_t brightness)
 {
-	const uint32_t colors[];
-	int length;
-	int slowness;
-} pattern_t;
+	uint32_t out=0;
+	uint32_t r;
 
-pattern_t patterns[]={
-	{
-		{0x00FF00,0xFF0000,0x0000FF},
-		3,
-		500
-	}
+	r=color & 0xFF;
+	r*=brightness;
+	r>>=8;
+	out |= r;
+
+	r=(color >> 8) & 0xFF;
+	r*=brightness;
+	r>>=8;
+	out |= r << 8;
+
+	r=(color >> 16) & 0xFF;
+	r*=brightness;
+	r>>=8;
+	out |= r << 16;
+	return out;
+}
+
+
+
+#define RAINBOW_SLOWNESS 25
+PROGMEM static const prog_uint32_t patterns[]=
+{
+	//4th of july
+	4,
+	500,0x0000FF,250,0xFFFFFF,500,0x00FF00,250,0xFFFFFF,
+	//rainbow (72)
+	72,
+	RAINBOW_SLOWNESS,0x0000ff,RAINBOW_SLOWNESS,0x0015ff,RAINBOW_SLOWNESS,0x002bff,RAINBOW_SLOWNESS,0x0040ff,RAINBOW_SLOWNESS,0x0055ff,
+	RAINBOW_SLOWNESS,0x006aff,RAINBOW_SLOWNESS,0x0080ff,RAINBOW_SLOWNESS,0x0095ff,RAINBOW_SLOWNESS,0x00aaff,RAINBOW_SLOWNESS,0x00bfff,
+	RAINBOW_SLOWNESS,0x00d5ff,RAINBOW_SLOWNESS,0x00eaff,RAINBOW_SLOWNESS,0x00ffff,RAINBOW_SLOWNESS,0x00ffea,RAINBOW_SLOWNESS,0x00ffd4,
+	RAINBOW_SLOWNESS,0x00ffbf,RAINBOW_SLOWNESS,0x00ffaa,RAINBOW_SLOWNESS,0x00ff95,RAINBOW_SLOWNESS,0x00ff80,RAINBOW_SLOWNESS,0x00ff6a,
+	RAINBOW_SLOWNESS,0x00ff55,RAINBOW_SLOWNESS,0x00ff40,RAINBOW_SLOWNESS,0x00ff2a,RAINBOW_SLOWNESS,0x00ff15,RAINBOW_SLOWNESS,0x00ff00,
+	RAINBOW_SLOWNESS,0x15ff00,RAINBOW_SLOWNESS,0x2bff00,RAINBOW_SLOWNESS,0x40ff00,RAINBOW_SLOWNESS,0x55ff00,RAINBOW_SLOWNESS,0x6aff00,
+	RAINBOW_SLOWNESS,0x80ff00,RAINBOW_SLOWNESS,0x95ff00,RAINBOW_SLOWNESS,0xaaff00,RAINBOW_SLOWNESS,0xbfff00,RAINBOW_SLOWNESS,0xd5ff00,
+	RAINBOW_SLOWNESS,0xeaff00,RAINBOW_SLOWNESS,0xffff00,RAINBOW_SLOWNESS,0xffea00,RAINBOW_SLOWNESS,0xffd400,RAINBOW_SLOWNESS,0xffbf00,
+	RAINBOW_SLOWNESS,0xffaa00,RAINBOW_SLOWNESS,0xff9500,RAINBOW_SLOWNESS,0xff8000,RAINBOW_SLOWNESS,0xff6a00,RAINBOW_SLOWNESS,0xff5500,
+	RAINBOW_SLOWNESS,0xff4000,RAINBOW_SLOWNESS,0xff2b00,RAINBOW_SLOWNESS,0xff1500,RAINBOW_SLOWNESS,0xff0000,RAINBOW_SLOWNESS,0xff0015,
+	RAINBOW_SLOWNESS,0xff002b,RAINBOW_SLOWNESS,0xff0040,RAINBOW_SLOWNESS,0xff0055,RAINBOW_SLOWNESS,0xff006a,RAINBOW_SLOWNESS,0xff0080,
+	RAINBOW_SLOWNESS,0xff0095,RAINBOW_SLOWNESS,0xff00aa,RAINBOW_SLOWNESS,0xff00bf,RAINBOW_SLOWNESS,0xff00d5,RAINBOW_SLOWNESS,0xff00ea,
+	RAINBOW_SLOWNESS,0xff00ff,RAINBOW_SLOWNESS,0xea00ff,RAINBOW_SLOWNESS,0xd400ff,RAINBOW_SLOWNESS,0xbf00ff,RAINBOW_SLOWNESS,0xaa00ff,
+	RAINBOW_SLOWNESS,0x9500ff,RAINBOW_SLOWNESS,0x8000ff,RAINBOW_SLOWNESS,0x6a00ff,RAINBOW_SLOWNESS,0x5500ff,RAINBOW_SLOWNESS,0x4000ff,
+	RAINBOW_SLOWNESS,0x2b00ff,RAINBOW_SLOWNESS,0x1500ff
 };
-static const int num_patterns=sizeof(patterns)/sizeof(patterns[0]);
 
-int iterate_pattern(int pid,int pattern_frame_id)
+static const int size_patterns=sizeof(patterns)/sizeof(patterns[0]);
+
+int iterate_pattern(int pptr,int pattern_frame_id,uint8_t brightness)
 {
-	pattern_t *selpattern=&patterns[pid % num_patterns];
-	const uint32_t *frames=selpattern->colors;
-	send_color_all(frames[pattern_frame_id % selpattern->length]);
-	delay(selpattern->slowness);
+	uint32_t len=pgm_read_dword_near(patterns+pptr);
+	
+	int pfidlu=pattern_frame_id << 1;
+	uint32_t slowness=pgm_read_dword_near(patterns+pptr+1+pfidlu);
+	uint32_t color=pgm_read_dword_near(patterns+pptr+2+pfidlu);
+	color=brighten(color,brightness);
+	send_color_all(color);
+	delay(slowness);
 	pattern_frame_id++;
+	if(pattern_frame_id >= len)
+	{
+		pattern_frame_id=0;
+	}
 	return pattern_frame_id;
-} 
+}
+
+int increment_pattern(int pptr)
+{
+	pptr+=(pgm_read_dword_near(patterns+pptr) << 1);
+	pptr++;
+	if(pptr >= size_patterns)
+	{
+		pptr=0;
+	}
+	return pptr;
+}
 
 int pframeid=0;
-int curpattern=0;
+int initpattern=1;
+int curptr=0;
+uint8_t curbrightness=255;
 
+void setup() 
+{              
+	for(int i=0;i<num_strips;i++)
+	{
+		strips[i]->setup();
+	}
+	for(int i=0;i<initpattern;i++)
+	{
+		curptr=increment_pattern(curptr);
+	}
+}
 void loop()
-{
-	pframeid=iterate_pattern(curpattern,pframeid);
+{	
+	pframeid=iterate_pattern(curptr,pframeid,curbrightness);
 }
